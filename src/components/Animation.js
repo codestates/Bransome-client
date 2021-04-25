d151f7482a05fd40a62cb3456035535f1941e08b
coderealsw <68404212+coderealsw@users.noreply.github.com>
2021-04-25 19:58:32 +0900

    add/animation

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,39 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/animation.css";
-import {
-  faGoogle,
-  faAirbnb,
-  faApple,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import netflix from "../images/motion_svg/netflix-icon-2016-1.svg";
+import spotify from "../images/motion_svg/spotify.svg";
+import mcdonald from "../images/motion_svg/mcdonald-s-15.svg";
+import google from "../images/motion_svg/google-icon.svg";
+import airbnb from "../images/motion_svg/airbnb-1.svg";
+import apple from "../images/motion_svg/apple.svg";
 
 const Animation = () => {
+  const [position, setPosition] = useState(0);
+  console.log(window.scrollY);
+
+  function onScroll() {
+    setPosition(window.scrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
     <div className="logo_animation">
       <div className="logo_animation_container">
         <div className="motion_box">
-          <div className="text_logo">
-            "branding"
+          <div
+            className="motion1"
+            style={{
+              transform: `translateX(${position / 3}px)`,
+            }}
+          >
+            Inspired
             <div className="logo">
-              <FontAwesomeIcon icon={faGoogle} />
+              <img src={airbnb} />
             </div>
-            "Inspired"
+            Branding
             <div className="logo">
-              <FontAwesomeIcon icon={faApple} />
+              <img src={spotify} />
             </div>
-            "branding"
+            Inspired
+            <div className="logo">
+              <img src={netflix} />
+            </div>
+            Branding
           </div>
 
-          <div className="text_logo">
-            "Google"
+          <div
+            className="motion2"
+            style={{
+              transform: `translateX(${-position / 5}px)`,
+            }}
+          >
+            Google
             <div className="logo">
-              <FontAwesomeIcon icon={faGoogle} />
+              <img src={google} />
             </div>
-            "apple"
+            Apple
             <div className="logo">
-              <FontAwesomeIcon icon={faApple} />
+              <img src={apple} />
             </div>
-            "Google"
+            Mcdonald
+            <div className="logo">
+              <img src={mcdonald} className="mc" />
+            </div>
+            Amazon
           </div>
         </div>
       </div>
